8118ef0edb24253f4d978b0604b61c1f808185a5
SoohoPark <clapp301@gmail.com>
2024-09-10 22:31:28 +0900

    [백준] 1629

---
+++ week1/Q_1629_곱셈.py
@@ -0,0 +1,21 @@
+import sys
+
+sys.stdin = open("z_input.text", "r")
+
+def input_():
+    return sys.stdin.readline().rstrip()
+
+A, B, C = map(int, input_().split(" "))
+
+def power(a, b):
+    if b == 1:
+        return a % C
+
+    half = power(a, b // 2)
+
+    if b % 2 == 0:
+        return (half * half) % C
+    else:
+        return (((half * half) % C) * a) % C
+
+print(power(A, B))
