b7df658516809b5e40ec1f8815a4fd99eebcd715
SoohoPark <clapp301@gmail.com>
2024-09-11 16:25:36 +0900

    [백준] 1655

---
+++ week1/Q_1655_가운데를말해요.py
@@ -0,0 +1,31 @@
+import sys
+import heapq
+
+sys.stdin = open("z_input.text", "r")
+
+def input_():
+    return sys.stdin.readline().rstrip()
+
+N = int(input_())
+
+max_heap = []
+min_heap = []
+
+for i in range(N):
+    value = int(input_())
+    if len(max_heap) == len(min_heap):
+        heapq.heappush(max_heap, -value)
+    else:
+        heapq.heappush(min_heap, value)
+
+    if not min_heap:
+        print(-max_heap[0])
+        continue
+
+    if -max_heap[0] > min_heap[0]:
+        b = -heapq.heappop(max_heap)
+        s = heapq.heappop(min_heap)
+        heapq.heappush(max_heap, -s)
+        heapq.heappush(min_heap, b)
+
+    print(-max_heap[0])
