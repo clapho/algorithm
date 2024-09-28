ee5420a3afb2bf5556507c1b60c2f0ea9465d2d6
SoohoPark <clapp301@gmail.com>
2024-09-28 19:32:00 +0900

    [백준] 1463

---
+++ week1/dynamic_programming/Q_1463_1로만들기.py
@@ -0,0 +1,16 @@
+import sys
+
+def input_():
+    return sys.stdin.readline().rstrip()
+
+N = int(input_())
+d = [0 for _ in range(N + 1)]
+
+for i in range(2, N + 1):
+    d[i] = d[i - 1] + 1
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3] + 1)
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2] + 1)
+
+print(d[N])
